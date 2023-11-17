e25b58815a01fe420fea772f5944ac7474d7ab8f
pedrosayuri <yuri.14psn@hotmail.com>
2023-11-17 17:43:31 -0300

    Token JWT Candidate

--- src/main/java/br/com/yuripedrosa/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/yuripedrosa/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package br.com.yuripedrosa.gestao_vagas.modules.candidate.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.yuripedrosa.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.yuripedrosa.gestao_vagas.modules.candidate.useCases.CreateCandidadeUseCase;
+import br.com.yuripedrosa.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private ProfileCandidateUseCase profileCandidateUseCase;
+    
     @Autowired
     private CreateCandidadeUseCase createCandidadeUseCase;
 
@@ -26,6 +34,20 @@ public class CandidateController {
         } catch(Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+        var idCandidate = request.getAttribute("candidate_id");
+        
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+
     }
 
 }
--- src/main/java/br/com/yuripedrosa/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/yuripedrosa/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -9,4 +9,5 @@ import br.com.yuripedrosa.gestao_vagas.modules.candidate.entities.CandidateEntit
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
 }
